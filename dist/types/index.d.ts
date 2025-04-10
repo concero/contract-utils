110bbf096582315f153443a27ec86d31f3bb2411
Oleg <olegkron.cc@gmail.com>
2025-04-10 19:48:18 +0100

    types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,5 @@
 import { config } from './constants/config';
-import { ConceroNetwork, conceroNetworks } from "./networks";
+import { ConceroNetwork, conceroNetworks, type ConceroMainnetNetworkNames, type ConceroTestnetNetworkNames } from './networks';
 import { networkTypes } from './networks/networkTypes';
-export { config, conceroNetworks, networkTypes, ConceroNetwork };
+export { ConceroNetwork, conceroNetworks, config, networkTypes };
+export type { ConceroMainnetNetworkNames, ConceroTestnetNetworkNames };
