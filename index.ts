70af6d3f64f00b160ceb61b28bb82e28ff1e793a
Oleg <olegkron.cc@gmail.com>
2025-04-10 19:09:46 +0100

    dist

--- index.ts
+++
@@ -1,6 +0,0 @@
-import {config} from './src/constants/config';
-import {conceroNetworks} from './src/networks';
-import {networkTypes} from './src/networks/networkTypes';
-import {type ConceroNetwork} from './src/types/ConceroNetwork';
-
-export {config, conceroNetworks, networkTypes, ConceroNetwork}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {config} from './constants/config';
+import {conceroNetworks} from './networks';
+import {networkTypes} from './networks/networkTypes';
+import {type ConceroNetwork} from './types/ConceroNetwork';
+
+export {config, conceroNetworks, networkTypes, ConceroNetwork}
