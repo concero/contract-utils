4f64ff5e4f9f2961c42ca446f7e04677da292cd1
Oleg <olegkron.cc@gmail.com>
2025-04-10 18:43:29 +0100

    index

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './src/constants/config';
+export * from './src/networks';
+export * from './src/networks/customViemChains';
+export * from './src/networks/networkTypes';
+export * from './src/types/ConceroNetwork';
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './constants/config';
-export * from './networks';
-export * from './networks/customViemChains';
-export * from './networks/networkTypes';
-export * from './types/ConceroNetwork';
